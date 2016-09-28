442138242314758f6ab6d42b8416c492dd439576
TheDen <deniskhoshaba@gmail.com>
2016-09-29 00:32:56 +1000

    parametrise stepsize

--- cursor.py
+++ cursor.py
@@ -6,14 +6,17 @@ import pyautogui
 x = pyautogui.position()[0]
 y = pyautogui.position()[1]
 step = random.randrange(5)
-speed =  0.1
+stepsize = 10
+speed =  0.01
 while True:
+ x = pyautogui.position()[0]
+ y = pyautogui.position()[1]
  step = random.randrange(5)
  if step == 0:
-   pyautogui.moveTo(x+1, y, duration=speed)
+   pyautogui.moveTo(x+stepsize, y, duration=speed)
  elif step == 1:
-   pyautogui.moveTo(x-1, y, duration=speed)
+   pyautogui.moveTo(x-stepsize, y, duration=speed)
  elif step == 2:
-   pyautogui.moveTo(x, y+1, duration=speed)
+   pyautogui.moveTo(x, y+stepsize, duration=speed)
  elif step == 3:
-   pyautogui.moveTo(x, y-1, duration=speed)
+   pyautogui.moveTo(x, y-stepsize, duration=speed)
