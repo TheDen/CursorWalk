4e42416509ea934da72ba51f4a9e189f72071525
TheDen <deniskhoshaba@gmail.com>
2016-10-16 13:32:51 +1100

    update options

--- cursor.py
+++ cursor.py
@@ -7,15 +7,15 @@ import argparse
 pyautogui.FAILSAFE =  False
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--stepsize', default=10, type=int, help="pixel stepsize")
-parser.add_argument('-s', '--speed', default=0.1, type=float, help="secons to move between pixels")
+parser.add_argument('-s', '--stepsize', default=10, type=int, help="pixel stepsize")
+parser.add_argument('-r', '--ratespeed', default=0.1, type=float, help="seconds between each pixel move")
 parser.add_argument('-t', '--timeout', default=0, type=float, help="timeout for idle mouse to start random walk")
 
 args = parser.parse_args()
 
-stepsize = args.stepsize
-speed =  args.speed
-timeout =  args.timeout
+stepsize = abs(args.stepsize)
+speed =  abs(args.ratespeed)
+timeout =  abs(args.timeout)
 
 def startwalk(stepsize, speed):
  while True:
